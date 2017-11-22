f61a1ade97102f9e8d0da1d6349b035b20c978f5
epsilon <esther1987@gmail.com>
2017-11-22 22:47:23 +0100

    code cleanup (mostly comments)

--- app_Easy_PH.py
+++ app_Easy_PH.py
@@ -4,35 +4,6 @@ import tkFileDialog
 
 from function_main import main_test, check_and_prepare_variables, main_function
 
-# from Tkinter import Tk
-# from tkFileDialog import askopenfilename
-
-# # https://stackoverflow.com/questions/3579568/choosing-a-file-in-python-with-simple-dialog
-# Tk.Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-# filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-# print(filename)
-
-
-# def browse_button(self, which="out"):
-#     # Allow user to select a directory and store it in global var
-#     # called folder_path
-#     # global folder_path
-#     filename = tkFileDialog.askdirectory()
-#     if which == "in":
-#         self.folder_path.set(filename)
-#     elif which == "out":
-#         self.folder_path2.set(filename)
-#     print(filename)
-
-
-# root = Tk()
-# folder_path = StringVar()
-# lbl1 = Label(master=root,textvariable=folder_path)
-# lbl1.grid(row=0, column=1)
-# button2 = Button(text="Browse", command=browse_button)
-# button2.grid(row=0, column=3)
-
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         tk.Frame.__init__(self, master)
@@ -72,17 +43,8 @@ class Application(tk.Frame):
         print(filename)
 
     def createWidgets(self):
-        
-        # ## si cliques s'obre askdirectory
-        # self.data_path = tk.Button(self,command=browse_button)
-        # self.data_path["text"] = "Folder data"
-        # self.data_path["fg"]   = "black"
-        # # self.data_path["command"] = tkFileDialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
-        # # self.data_path["command"] = tkFileDialog.askdirectory()
-        # # self.data_path.pack({"side": "left"})
-        # self.data_path.grid(row=0, column=0)
         ###################################################################
-        ## new 
+        ## select folder data
         self.folder_path_input = tk.StringVar()
         self.label_folder1 = tk.Label(self,textvariable=self.folder_path_input)
         self.label_folder1.grid(row=1, column=0)
@@ -112,7 +74,6 @@ class Application(tk.Frame):
         self.execute_button = tk.Button(self,command=self.launch_computation)
         self.execute_button["text"] = "Run program"
         self.execute_button["fg"]   = "blue"
-        # self.data_path.pack({"side": "left"})
         self.execute_button.grid(row=0, column=3)
         ## ----------------------------------------------------------
 
@@ -123,16 +84,7 @@ class Application(tk.Frame):
         self.lab_results.grid(row=2, column=2)
         ## ------------- OPTIONAL ----------------------------------
         ## select specific file to analyse
-        # ################################################################### old 
-        # self.data_file = tk.Button(self)
-        # self.data_file["text"] = "File to analyse"
-        # self.data_file["fg"]   = "black"
-        # # self.data_file["command"]=self.askopenfilename()
         # # self.data_file = tk.askopenfilename(initialdir = "C:/<whatever>")
-        # # self.data_file["command"] =  self.askopenfilename()
-        # self.data_file.grid(row=0, column=1)
-        ################################################################### 
-        ## new 
         self.file_path_input = tk.StringVar()
         self.label_folder3 = tk.Label(self,textvariable=self.file_path_input)
         self.label_folder3.grid(row=1, column=1)
@@ -141,14 +93,6 @@ class Application(tk.Frame):
 
         ## ----------------------------------------------------------
         ## output data path
-        # ################################################################### old 
-        # self.output_folder = tk.Button(self)
-        # self.output_folder["text"] = "Output folder"
-        # self.output_folder["fg"]   = "black"
-        # # self.output_folder["command"] =  self.askopenfilename()
-        # self.output_folder.grid(row=0, column=2)
-        ###################################################################
-        # new 
         # \todo add output folder as optional and data folder as default
         # \todo add input folder starting path from: on sigui
         self.folder_path_output = tk.StringVar()
@@ -157,26 +101,18 @@ class Application(tk.Frame):
         self.button_output_path = tk.Button(self,text="Output folder", command=self.browse_button_output)
         self.button_output_path.grid(row=0, column=2)
 
-
-
         ## ----------------------------------------------------------
         ## generate plots
         self.plots_on = tk.BooleanVar()
         self.plots_on.set(True)
         self.plots = tk.Checkbutton(self,text ='Generate Plots',variable = self.plots_on)
         self.plots.grid(row=3, column=2,sticky=tk.W)
-        # self.plots
-        # \todo ficar per defecte True
-        # self.plots["command"] =  self.askopenfilename()
         ## ----------------------------------------------------------
         ## plots normalized
         self.plots_norm = tk.BooleanVar()
         self.plots_norm.set(False)
         self.plots_n = tk.Checkbutton(self,text ='Normalized plots',variable = self.plots_norm)
         self.plots_n.grid(row=4, column=2,sticky=tk.W)
-        # self.plots
-        # \todo ficar per defecte True
-        # self.plots["command"] =  self.askopenfilename()
         ###################################################################
         # max dimension to compute persistent homology
         self.lab_dim_max = tk.Label(self,text="Max dimension to compute\n persistent homology",
@@ -226,7 +162,7 @@ class Application(tk.Frame):
             print 'Go to check your results at %s/results!'%self.folder_path_input.get()
         else:
             print 'Go to check your results at %s/results!'%output_path
-        return()
+        
 
 
 
@@ -239,3 +175,4 @@ root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
 
+# root.quit()
